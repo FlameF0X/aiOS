22b67dd617c3333a57a7e3b2061634fdead25167
FlameF0X <161115754+FlameF0X@users.noreply.github.com>
2025-07-31 16:57:46 +0000

    Implement minimal CLI shell: print prompt, read key, echo input in kernel

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,12 +1,41 @@
 // aiOS Minimal Kernel (kernel.c)
-// Prints a message to the screen (VGA text mode)
+// Minimal CLI shell: print prompt, read keypresses, echo input, repeat
+
+// Print a string to VGA text mode
+void print(const char* str, int* col) {
+    volatile char* video = (volatile char*)0xb8000;
+    while (*str) {
+        video[(*col) * 2] = *str;
+        video[(*col) * 2 + 1] = 0x0F;
+        (*col)++;
+        str++;
+    }
+}
+
+// Read a character from keyboard using BIOS interrupt 0x16
+char getch() {
+    char c;
+    __asm__ __volatile__ (
+        "mov $0x00, %%ah\n\t"
+        "int $0x16\n\t"
+        "mov %%al, %0\n\t"
+        : "=r"(c)
+        :
+        : "ax"
+    );
+    return c;
+}
 
 void kernel_main() {
-    volatile char* video = (volatile char*)0xb8000;
-    const char* msg = "aiOS Kernel: Hello from the kernel!";
-    for (int i = 0; msg[i] != 0; ++i) {
-        video[i * 2] = msg[i];
-        video[i * 2 + 1] = 0x0F; // White on black
+    int col = 0;
+    print("aiOS CLI Shell> ", &col);
+    while (1) {
+        char c = getch();
+        if (c == '\r') {
+            print("\n\raiOS CLI Shell> ", &col);
+        } else {
+            char s[2] = {c, 0};
+            print(s, &col);
+        }
     }
-    while (1) {}
 }
